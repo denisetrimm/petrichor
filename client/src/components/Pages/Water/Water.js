0da6293ed07c663e533701fb0a444e2b0935d725
Denise Trimm <denise.araujo.trimm@gmail.com>
2022-09-02 21:59:36 -0400

    Add rendering logic to water page

--- client/src/components/Pages/Water/Water.js
+++ client/src/components/Pages/Water/Water.js
@@ -1,14 +1,78 @@
+//STYLING
 import styled from "styled-components";
+// ICONS
+
+//HOOKS & CONTEXT
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect, useContext } from "react";
+import { PlantContext } from "../../../context/PlantContext";
+import { UserContext } from "../../../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import moment from 'moment';
+//COMPONENTS
+import Card from "../../UI/Card";
+import WaterPlantCard from "./WaterPlantCard";
 import BackArrow from "../../UI/BackArrow";
 
 
 const Water = () => {
+    const { user, isAuthenticated, isLoading} = useAuth0();
+    const {plantUser} = useContext(UserContext);
+    const { allPlants, filteredPlants, handleClear }= useContext(PlantContext);
+    const [dueForWater, setDueForWater] = useState([]);
+    const navigate = useNavigate();
+
+    const handlePlantClick = (plantId) => {
+        console.log("clicked!");
+        console.log(`plantId`)
+        handleClear();
+        navigate(`/plants/${plantId}`);
+    }
+    
+    useEffect(()=> {
+        if(plantUser && plantUser.housePlants.length > 0){
+            const filterPlants = plantUser.housePlants.filter(plant => {
+                return moment().isSameOrAfter(plant.nextWatering, "day")
+            })
+            if (filterPlants.length > 0) {
+                setDueForWater(filterPlants)
+            }
+        }
+    },[plantUser])
+
     return (
         <>
-        <BackArrow/>
-        Water
+
+        <h2>Water</h2>
+        {plantUser &&
+            <BackArrow/>
+        }
+        {allPlants && dueForWater.length > 0 &&
+        <>  
+            <PlantGrid>
+                {dueForWater.map(plant => {
+                        return (
+                            <Card key={plant.plantId} id={plant.plantId} handleFunction={handlePlantClick}>
+                                <WaterPlantCard plant={plant}/>
+                            </Card>
+                        )
+                    }
+                )}
+            </PlantGrid>
+        </>
+        }
         </>
     );
 }
+
+const PlantGrid = styled.div`
+    /* border: 1px solid blue; */
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 15px 1%;
+    margin-top: 40px;
+    width: 100%;
+`
 
 export default Water;
---
+++ client/src/components/Pages/Water/WaterPlantCard.js
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import { UserContext } from "../../../context/UserContext";
+import { useAuth0 } from "@auth0/auth0-react";
+
+
+const WaterPlantCard = ({plant}) => {
+    const { user, isAuthenticated, isLoading, logout} = useAuth0();
+    const { plantUser } = useContext(UserContext);
+
+    return (
+        <>
+        {/* <Wrapper> */}
+            <PlantImg src={plant.imgSrc}/>
+            <CommonName>{plant.commonName}</CommonName>
+            <BotanicalName>{plant.botanicalName}</BotanicalName>
+        {/* </Wrapper> */}
+        </>
+    );
+}
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+`
+const PlantImg = styled.img`
+    border-radius: 8px;
+    max-width: 80%;
+    max-height: 80%;
+`
+const CommonName = styled.p`
+    font-weight: bold;
+    padding: 10px 0;
+`
+const BotanicalName = styled.p`
+    font-style: italic;
+    font-size: 14px;
+`
+export default WaterPlantCard;
